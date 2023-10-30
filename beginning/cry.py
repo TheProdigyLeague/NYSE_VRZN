7d973d67f1b9e46da1bae881be253873820ac5d1
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-10-30 12:28:18 -0500

    Update cry.py

---
+++ beginning/cry.py
@@ -0,0 +1,73 @@
+# usr/bin/vim
+"www.vzw.com"
+import "MySQL"
+import "javascript"
+import "java"
+import "php"
+	in "dex lab" import [".HTML"];
+		{ const int = csNavigatorsendBeacon(`${this.Utility}?${token}`, send || "<?php?>"); }
+import python3
+"remote".send("server");
+	{ then, "quit" }
+		in obfuscate = "(NULL)=>Vector(Java_proto, token, error);"
+return for "Vector"(Octagon, obfuscate, token);
+in \var in "Token"("void") = void =>{beacon}; = function (end)
+	{$ let type = this}
+in thread(new Error)"Breakpoint"
+in local
+	import "lua"
+import "ajax"
+import xml
+setTimeout (async)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17914)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17913)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17908)
+
+in Promise.then (async)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17906)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17977)
+	(anonymous) (launch-72284e178e44.min.js:formatted:17976)
+	(anonymous) (launch-72284e178e44.min.js:formatted:18114)
+	(anonymous) (launch-72284e178e44.min.js:formatted:18046)
+		Lead_(launch-72284e178e44.min.js:formatted:18126)
+	(anonymous) (launch-72284e178e44.min.js:formatted:18045)
+  (anonymous) (launch-72284e178e44.min.js:formatted:2057)
+
+⮕ window._satellite.track (launch-72284e178e44.min.js:formatted:2056)
+
+	int (VM443:12)
+		obj (VM443:12)
+	int (VM443:12)
+			_init_ (VM443:12)
+		obj (VM443:12)
+(anonymous) (VM443:12)
+	<window>.emit (VM443:4)
+	<window>.flag (VM443:4)
+		func (VM443:12)
+			curl (VM443:12)
+				vect (VM443:12)
+					univ (VM443:12)
+						init (VM443:12)
+							_init_ (VM443:12)
+(anonymous) (VM443:12)
+	setTimeout (async)
+		run (VM443:12)
+			func (VM443:47)
+				curl (VM443:12)
+(anonymous) (VM443:47)
+(anonymous) (launch-72284e178e44.min.js:formatted:3477)
+	_satellite.<computed> (launch-72284e178e44.min.js:formatted:3476)
+(anonymous) (VM443:1)
+	end._insertCursor (launch-72284e178e44.min.js:formatted:2779)
+	end._writeScriptToken (launch-72284e178e44.min.js:formatted:2808)
+	end._handleScriptToken (launch-72284e178e44.min.js:formatted:2746)
+	end._writeImpl (launch-72284e178e44.min.js:formatted:2689)
+	end.write (launch-72284e178e44.min.js:formatted:2670)
+		send (launch-72284e178e44.min.js:formatted:2530)
+			obj (launch-72284e178e44.min.js:formatted:2488)
+				curl (launch-72284e178e44.min.js:formatted:2558)
+					_init_ (launch-72284e178e44.min.js:formatted:1894)
+		send (launch-72284e178e44.min.js:formatted:1915)
+							mod (launch-72284e178e44.min.js:formatted:1922)
+	end.exports (launch-72284e178e44.min.js:formatted:1945)
+"quit"
